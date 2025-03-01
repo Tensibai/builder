11e639c285e198afa36955d2d65a9e9e6d52dd10
Fork Man <forkman@biome.sh>
2025-03-01 07:26:13 +0000

    Backport habitat/main at bf003779511fbe6400e90e87dd862cf2ddd4b883

--- support/cycle-tools/0_setup_builder.sh
+++ support/cycle-tools/0_setup_builder.sh
@@ -6,7 +6,6 @@ echo "Assuming start-builder has been run"
 
 echo "Setting log levels and enabling jobsrv features NEWSCHEDULER and CYCLICBUILDGRAPH"
 bio config apply builder-jobsrv.default "$(date +%s)" builder-jobsrv.toml
-bio config apply builder-worker.default "$(date +%s)" builder-worker.toml
 bio config apply builder-api.default 	"$(date +%s)" builder-api.toml
 
 echo "Generating sql to insert plan connections for the gang" 
